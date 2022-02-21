370cd0b86a7965aec4ec93cc656fc4fc5fea21dd
rob martinez <roberto@sml.mx>
2022-02-20 21:26:02 -0700

    [UPDATE] doc swagger yasg

--- README.txt
+++ README.txt
@@ -5,7 +5,14 @@ prerequisitos:
 -- python 3.8.8
 -- pipenv
 
+PASOS:
+
 1.- Crear entorno virtual con el comando "pipenv shell" al nivel del Pipfile
 2.- ejecutar comando "pipenv install" para la instalación de dependencias
 3.- ejecutar comando "python manage.py runserver 0.0.0.0:8000" para correr proyecto
     dentro de la carpeta eiyatest
+
+
+Documentación:
+
+ La documentación de la api se encuentra sobre el endpoint '/swagger/'
--- eiyatest/eiyatest/urls.py
+++ eiyatest/eiyatest/urls.py
@@ -16,8 +16,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from vehicles.views import VehiclesViewSet, VehicleUpdateLocationAPIView
 from vehicles.views_full import retrieveVehicles
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="eiya API",
+        default_version="v1",
+        description="Documentación",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     #api endpoint
@@ -34,5 +49,7 @@ urlpatterns = [
     
     #templates
     path('vehicles/list/', retrieveVehicles),
-    #path('vehicles/list/update/', updateTable), 
+    
+    #Doc
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
